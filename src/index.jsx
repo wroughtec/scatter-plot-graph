5a6e3b14c45fd4a515277064499933796e712268
William Corry <william@wrought-webdesign.co.uk>
2018-11-27 09:07:42 +0000

    removed unused libs

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 // @flow
 import React from 'react';
 import { render } from 'react-dom';
-import ReactChartkick from 'react-chartkick';
-import Chart from 'chart.js';
+
 import { App } from './client/App';
 
-ReactChartkick.addAdapter(Chart);
 
 const rootElement = document.getElementById('root');
 
